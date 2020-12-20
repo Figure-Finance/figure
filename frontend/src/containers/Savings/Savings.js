0621617c966b4c47bd994296904736a2aa100082
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:41:57 -0800

    [create] Savings container

---
+++ frontend/src/containers/Savings/Savings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Savings = props => {
+  return (
+    <div>
+      <Navbar active='s' />
+    </div>
+  )
+}
+
+export default Savings
