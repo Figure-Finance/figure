c28b4555b053f0f1d9615e90c45252d165feaedc
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:42:09 -0800

    [create] Profile container

---
+++ frontend/src/containers/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Profile = props => {
+  return (
+    <div>
+      <Navbar active='p' />
+    </div>
+  )
+}
+
+export default Profile
