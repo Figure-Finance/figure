fb17ba107a5cc30bb06c4068fe89e059d6e63022
Starlight Romero <starlightromero@gmail.com>
2020-12-19 22:33:51 -0800

    [create] Logo component

---
+++ frontend/src/components/UI/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Logo.module.css'
+import { ReactComponent as LogoIcon } from '../../../assets/logo.svg'
+
+const Logo = props => {
+  return (
+    <LogoIcon className={classes.Logo} />
+  )
+}
+
+export default Logo
