c0c9b9a915a72c6cbbe857a8b21f7836263c91a5
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:41:44 -0800

    [create] Yearly container

---
+++ frontend/src/containers/Yearly/Yearly.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Yearly = props => {
+  return (
+    <div>
+      <Navbar active='y' />
+    </div>
+  )
+}
+
+export default Yearly
