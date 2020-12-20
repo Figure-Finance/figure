c5a21d3b42347e91592f474e5a07ab9469adcaca
Starlight Romero <starlightromero@gmail.com>
2020-12-19 23:15:33 -0800

    [update] added Route for Weekly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Auth from './containers/Auth/Auth'
+import Weekly from './containers/Weekly/Weekly'
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path='/' component={Auth} />
+      <Route path='/signup' component={Auth} />
+      <Route exact path='/' component={Weekly} />
       <Redirect to='/' />
     </Switch>
   )
