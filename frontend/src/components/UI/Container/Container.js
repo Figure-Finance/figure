c3d4d436580ce5c5f515e20c0892fc15f539e30a
Starlight Romero <starlightromero@gmail.com>
2020-12-20 13:08:32 -0800

    [update[ added width and height props

--- frontend/src/components/UI/Container/Container.js
+++ frontend/src/components/UI/Container/Container.js
@@ -4,14 +4,18 @@ import classes from './Container.module.css'
 
 const Container = props => {
   return (
-    <div className={classes.Container}>
+    <div
+      className={classes.Container}
+      style={{ width: props.width, height: props.height }}>
       {props.children}
     </div>
   )
 }
 
 Container.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
+  height: PropTypes.string,
+  width: PropTypes.string
 }
 
 export default Container
