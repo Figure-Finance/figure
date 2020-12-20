72938372c09198d315ebbd0284ab29484626b83a
Starlight Romero <starlightromero@gmail.com>
2020-12-19 23:14:53 -0800

    [create] Weekly container

---
+++ frontend/src/containers/Weekly/Weekly.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Weekly = props => {
+  return (
+    <div>
+      <Navbar active='w' />
+    </div>
+  )
+}
+
+export default Weekly
