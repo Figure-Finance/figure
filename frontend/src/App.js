8a480cd6e0b4751eb05db55f0bfec1fcc373d9bc
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:44:10 -0800

    [update] Routes for Profile, Savings, Yearly, Monthly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Auth from './containers/Auth/Auth'
 import Weekly from './containers/Weekly/Weekly'
+import Monthly from './containers/Monthly/Monthly'
+import Yearly from './containers/Yearly/Yearly'
+import Savings from './containers/Savings/Savings'
+import Profile from './containers/Profile/Profile'
 
 const App = () => {
   return (
     <Switch>
-      <Route path='/signup' component={Auth} />
+      <Route exact path='/signup' component={Auth} />
+      <Route exact path='/profile' component={Profile} />
+      <Route exact path='/savings' component={Savings} />
+      <Route exact path='/yearly' component={Yearly} />
+      <Route exact path='/monthly' component={Monthly} />
       <Route exact path='/' component={Weekly} />
       <Redirect to='/' />
     </Switch>
