e6f4c409d5d35d7d6ba0754a0d60189048795eaf
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:42:17 -0800

    [create] Monthly container

---
+++ frontend/src/containers/Monthly/Monthly.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Monthly = props => {
+  return (
+    <div>
+      <Navbar active='m' />
+    </div>
+  )
+}
+
+export default Monthly
