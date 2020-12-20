dbb9bb867f40a51e9efb830e180337363bada348
Starlight Romero <starlightromero@gmail.com>
2020-12-19 22:34:27 -0800

    [update] moved Container and Logo to components

--- frontend/src/containers/Auth/Auth.js
+++ frontend/src/containers/Auth/Auth.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import classes from './Auth.module.css'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
+import Container from '../../components/UI/Container/Container'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
+import Logo from '../../components/UI/Logo/Logo'
 
 const Auth = props => {
   return (
     <div className={classes.Auth}>
-      <div className={classes.Inset}>
+      <Container>
         <Input placeholder='Email' />
         <Input placeholder='Password' />
         <Button color='primary' size='large'>Sign Up</Button>
-      </div>
+      </Container>
       <div>
         <Logo className={classes.Logo} />
       </div>
