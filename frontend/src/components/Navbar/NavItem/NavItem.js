d2e2d8ad5677c34ad9abed3651a6aed8edaa00d1
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:43:53 -0800

    [create] NavItem component

---
+++ frontend/src/components/Navbar/NavItem/NavItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import classes from './NavItem.module.css'
+import Button from '../../UI/Button/Button'
+
+const NavItem = props => {
+  return (
+    <div className={classes.NavItem}>
+      <NavLink to={props.link}>
+        <Button
+          color='primary'
+          size='square'
+          active={props.active}>
+          {props.children}
+        </Button>
+      </NavLink>
+    </div>
+  )
+}
+
+NavItem.propTypes = {
+  children: PropTypes.string,
+  active: PropTypes.bool,
+  link: PropTypes.string
+}
+
+export default NavItem
