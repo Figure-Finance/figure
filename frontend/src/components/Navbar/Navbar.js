b028676d738579ace8aa2d4cbbaaf3e66705fcb1
Starlight Romero <starlightromero@gmail.com>
2020-12-20 11:43:34 -0800

    [update] Navbar with NavItems

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,17 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './Navbar.module.css'
 import Container from '../UI/Container/Container'
-import Button from '../UI/Button/Button'
+import NavItem from './NavItem/NavItem'
 
 const Navbar = props => {
   return (
     <div className={classes.Navbar}>
       <Container>
-        <Button color='primary' size='square' active>W</Button>
-        <Button color='primary' size='square'>M</Button>
-        <Button color='primary' size='square'>Y</Button>
-        <Button color='primary' size='square'>S</Button>
-        <Button color='primary' size='square'>P</Button>
+        <NavItem
+          link='/'
+          active={props.active === 'w'}>
+          W
+        </NavItem>
+        <NavItem
+          link='/monthly'
+          active={props.active === 'm'}>
+          M
+        </NavItem>
+        <NavItem
+          link='/yearly'
+          active={props.active === 'y'}>
+          Y
+        </NavItem>
+        <NavItem
+          link='/savings'
+          active={props.active === 's'}>
+          S
+        </NavItem>
+        <NavItem
+          link='/profile'
+          active={props.active === 'p'}>
+          P
+        </NavItem>
       </Container>
     </div>
   )
