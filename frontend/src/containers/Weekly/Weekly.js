6da1608792b363f4d97d6c9dcf6180427a2e3f83
Starlight Romero <starlightromero@gmail.com>
2020-12-20 13:08:17 -0800

    [update] added IncomeExpenseProgress

--- frontend/src/containers/Weekly/Weekly.js
+++ frontend/src/containers/Weekly/Weekly.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import IncomeExpenseProgress from '../../components/IncomeExpenseProgress/IncomeExpenseProgress'
 import Navbar from '../../components/Navbar/Navbar'
 
 const Weekly = props => {
   return (
     <div>
+      <IncomeExpenseProgress />
       <Navbar active='w' />
     </div>
   )
