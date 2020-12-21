7ac59b44beca6556c92b478e656fae7071561c5b
Starlight Romero <starlightromero@gmail.com>
2020-12-20 22:27:58 -0800

    [create] LeftArrow component

---
+++ frontend/src/components/Icons/LeftArrow/LeftArrow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as Left } from '../../../assets/left.svg'
+
+const LeftArrow = props => {
+  return (
+    <Left />
+  )
+}
+
+export default LeftArrow
