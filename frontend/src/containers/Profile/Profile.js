9d245f3604fef43f349535ab6cfbcd767ad94418
Starlight Romero <starlightromero@gmail.com>
2020-12-20 22:28:55 -0800

    [update] Profile container

--- frontend/src/containers/Profile/Profile.js
+++ frontend/src/containers/Profile/Profile.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import classes from './Profile.module.css'
+import Summary from '../../components/Summary/Summary'
 import Navbar from '../../components/Navbar/Navbar'
 
 const Profile = props => {
   return (
-    <div>
+    <div className={classes.Profile}>
+      <div className={classes.Main}>
+        <Summary color='primary' />
+        <Summary color='primary' />
+        <Summary color='danger' />
+      </div>
       <Navbar active='p' />
     </div>
   )
