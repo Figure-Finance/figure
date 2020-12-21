de45133fdcb6ca8dad21034578bcbb957c260292
Starlight Romero <starlightromero@gmail.com>
2020-12-20 19:02:41 -0800

    [update] Monthly container

--- frontend/src/containers/Monthly/Monthly.js
+++ frontend/src/containers/Monthly/Monthly.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import classes from './Monthly.module.css'
+import IncomeExpenseProgress from '../../components/IncomeExpenseProgress/IncomeExpenseProgress'
 import Navbar from '../../components/Navbar/Navbar'
+import Container from '../../components/UI/Container/Container'
+import Summary from '../../components/Summary/Summary'
 
 const Monthly = props => {
   return (
-    <div>
+    <div className={classes.Monthly}>
+      <IncomeExpenseProgress />
+      <div className={classes.Main}>
+        <Summary color='primary' />
+        <Container height='100%' width='33%' />
+        <Summary color='danger' />
+      </div>
       <Navbar active='m' />
     </div>
   )
