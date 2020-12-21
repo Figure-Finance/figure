005d322c696ffefc50055b520478b5d557f7da98
Starlight Romero <starlightromero@gmail.com>
2020-12-20 22:25:46 -0800

    [update] Savings component

--- frontend/src/containers/Savings/Savings.js
+++ frontend/src/containers/Savings/Savings.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import classes from './Savings.module.css'
+import GraphSummary from '../../components/GraphSummary/GraphSummary'
+import ProgressSummary from '../../components/ProgressSummary/ProgressSummary'
+import Summary from '../../components/Summary/Summary'
 import Navbar from '../../components/Navbar/Navbar'
 
 const Savings = props => {
   return (
-    <div>
+    <div className={classes.Savings}>
+      <ProgressSummary />
+      <div className={classes.Main}>
+        <GraphSummary />
+        <Summary color='neutral' />
+      </div>
       <Navbar active='s' />
     </div>
   )
