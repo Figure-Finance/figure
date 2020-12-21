50c350a2cd9dc70a3a6cb8ae4b8b95579db226c4
Starlight Romero <starlightromero@gmail.com>
2020-12-21 10:13:55 -0800

    [update] added two-way binding

--- frontend/src/components/UI/Input/Input.js
+++ frontend/src/components/UI/Input/Input.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classes from './Input.module.css'
 
 const Input = props => {
+  const [value, setValue] = useState('')
+
   return (
     <input
       type='text'
+      value={value}
       placeholder={props.placeholder}
-      className={classes.Input} />
+      className={classes.Input}
+      style={{ width: props.width || '100%' }}
+      onChange={e => setValue(e.target.value)} />
   )
 }
 
 Input.propTypes = {
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  width: PropTypes.string
 }
 
 export default Input
