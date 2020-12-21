454d86b8b60bef2fab9ca3d641d9971eacbe843b
Starlight Romero <starlightromero@gmail.com>
2020-12-21 10:13:13 -0800

    [update] with canAdd prop

--- frontend/src/components/Summary/Summary.js
+++ frontend/src/components/Summary/Summary.js
@@ -2,9 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './Summary.module.css'
 import Container from '../UI/Container/Container'
+import AddButton from '../UI/AddButton/AddButton'
 import Button from '../UI/Button/Button'
 
 const Summary = props => {
+  let addButton = null
+
+  if (props.canAdd) {
+    addButton = <AddButton color={props.color} />
+  }
+
   return (
     <Container
       height={props.height || '100%'}
@@ -13,30 +20,33 @@ const Summary = props => {
         <h1 className={props.color}>
           {props.title ? props.title : 'Types'}
         </h1>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$89.32'
-          dual>
-          Web Design
-        </Button>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$50.00'
-          dual>
-          Video Team
-        </Button>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$50.00'
-          dual>
-          Cirriculum
-        </Button>
+        <div className={props.canAdd ? classes.Buttons : classes.FullButtons}>
+          <Button
+            color={props.color}
+            size='thin'
+            width='90%'
+            secondary='$89.32'
+            dual>
+            Web Design
+          </Button>
+          <Button
+            color={props.color}
+            size='thin'
+            width='90%'
+            secondary='$50.00'
+            dual>
+            Video Team
+          </Button>
+          <Button
+            color={props.color}
+            size='thin'
+            width='90%'
+            secondary='$50.00'
+            dual>
+            Cirriculum
+          </Button>
+        </div>
+        {addButton}
       </div>
     </Container>
   )
@@ -46,7 +56,8 @@ Summary.propTypes = {
   color: PropTypes.string,
   height: PropTypes.string,
   width: PropTypes.string,
-  title: PropTypes.string
+  title: PropTypes.string,
+  canAdd: PropTypes.bool
 }
 
 export default Summary
