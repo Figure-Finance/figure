e72b2100ced4e38fd79c9a2fb458cdf0bdee38e3
Starlight Romero <starlightromero@gmail.com>
2020-12-21 10:12:45 -0800

    [update] LeftArrow with styles

--- frontend/src/components/Icons/LeftArrow/LeftArrow.js
+++ frontend/src/components/Icons/LeftArrow/LeftArrow.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { ReactComponent as Left } from '../../../assets/left.svg'
 
-const LeftArrow = props => {
-  return (
-    <Left />
-  )
+const LeftArrow = props => (
+  <Left style={{ width: '30px', fill: props.fill, marginTop: '6px', marginRight: '6px' }} />
+)
+
+LeftArrow.propTypes = {
+  fill: PropTypes.string
 }
 
 export default LeftArrow
