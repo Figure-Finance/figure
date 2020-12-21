579567667cbd3b255628a314ac130b691655c952
Starlight Romero <starlightromero@gmail.com>
2020-12-20 22:28:08 -0800

    [create] RightArrow component

---
+++ frontend/src/components/Icons/RightArrow/RightArrow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as Right } from '../../../assets/right.svg'
+
+const RightArrow = props => {
+  return (
+    <Right />
+  )
+}
+
+export default RightArrow
