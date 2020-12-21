408fb6aac78d98a544b9db6a91e76e5eca134f38
Starlight Romero <starlightromero@gmail.com>
2020-12-21 10:13:00 -0800

    [update] with styles

--- frontend/src/components/Icons/RightArrow/RightArrow.js
+++ frontend/src/components/Icons/RightArrow/RightArrow.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { ReactComponent as Right } from '../../../assets/right.svg'
 
-const RightArrow = props => {
-  return (
-    <Right />
-  )
+const RightArrow = props => (
+  <Right style={{ width: '30px', fill: props.fill, marginTop: '6px' }} />
+)
+
+RightArrow.propTypes = {
+  fill: PropTypes.string
 }
 
 export default RightArrow
