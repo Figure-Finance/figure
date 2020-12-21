48a7d2d705959acb8337eaf1e73c57889ad99b7a
Starlight Romero <starlightromero@gmail.com>
2020-12-20 22:50:16 -0800

    [create] Progress component

---
+++ frontend/src/components/ProgressSummary/ProgressBar/Progress/Progress.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './Progress.module.css'
+
+const Progress = props => {
+  const classNames = [classes.Progress]
+
+  if (props.position === 'left') {
+    classNames.push(classes.Left)
+  } else if (props.position === 'right') {
+    classNames.push(classes.Right)
+  }
+
+  if (props.color === 'primary') {
+    classNames.push(classes.Primary)
+  } else if (props.color === 'neutral') {
+    classNames.push(classes.Neutral)
+  } else if (props.color === 'danger') {
+    classNames.push(classes.Danger)
+  }
+
+  return (
+    <div
+      className={classNames.join(' ')}
+      style={{ width: `${props.percent}%` }} />
+  )
+}
+
+Progress.propTypes = {
+  color: PropTypes.string,
+  percent: PropTypes.string,
+  position: PropTypes.string
+}
+
+export default Progress
