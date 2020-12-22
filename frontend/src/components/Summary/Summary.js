c7edde20350fef49a119a8e283b3cc4b0a792934
Starlight Romero <starlightromero@gmail.com>
2020-12-22 15:18:00 -0800

    [update] buttons.map

--- frontend/src/components/Summary/Summary.js
+++ frontend/src/components/Summary/Summary.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classes from './Summary.module.css'
-import SummaryModal from '../SummaryModal/SummaryModal'
+import SummaryModal from './SummaryModal/SummaryModal'
 import Container from '../UI/Container/Container'
 import AddButton from '../UI/AddButton/AddButton'
 import Button from '../UI/Button/Button'
@@ -23,36 +23,25 @@ const Summary = props => {
     addButton = <AddButton color={props.color} clicked={openModal} />
   }
 
+  const buttons = props.content.map(item => (
+    <Button
+      key={item.type}
+      color={props.color}
+      size='thin'
+      width='90%'
+      secondary={`$${item.amount.toFixed(2)}`}
+      dual>
+      {item.type}
+    </Button>
+  ))
+
   let content = (
     <>
       <h1 className={props.color}>
         {props.title ? props.title : 'Types'}
       </h1>
       <div className={props.canAdd ? classes.Buttons : classes.FullButtons}>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$89.32'
-          dual>
-          Web Design
-        </Button>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$50.00'
-          dual>
-          Video Team
-        </Button>
-        <Button
-          color={props.color}
-          size='thin'
-          width='90%'
-          secondary='$50.00'
-          dual>
-          Cirriculum
-        </Button>
+        {buttons}
       </div>
       {addButton}
     </>
@@ -82,7 +71,8 @@ Summary.propTypes = {
   height: PropTypes.string,
   width: PropTypes.string,
   title: PropTypes.string,
-  canAdd: PropTypes.bool
+  canAdd: PropTypes.bool,
+  content: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default Summary
