c8070782d1a75cd078253785623ea0c7eb64a05d
Starlight Romero <starlightromero@gmail.com>
2020-12-21 17:41:42 -0800

    [update] Summary component

--- frontend/src/components/Summary/Summary.js
+++ frontend/src/components/Summary/Summary.js
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classes from './Summary.module.css'
+import Modal from '../Modal/Modal'
 import Container from '../UI/Container/Container'
 import AddButton from '../UI/AddButton/AddButton'
 import Button from '../UI/Button/Button'
 
 const Summary = props => {
+  const [showModal, setShowModal] = useState(false)
+
   let addButton = null
 
+  const openModal = () => {
+    setShowModal(true)
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
   if (props.canAdd) {
-    addButton = <AddButton color={props.color} />
+    addButton = <AddButton color={props.color} clicked={openModal} />
+  }
+
+  let content = (
+    <>
+      <h1 className={props.color}>
+        {props.title ? props.title : 'Types'}
+      </h1>
+      <div className={props.canAdd ? classes.Buttons : classes.FullButtons}>
+        <Button
+          color={props.color}
+          size='thin'
+          width='90%'
+          secondary='$89.32'
+          dual>
+          Web Design
+        </Button>
+        <Button
+          color={props.color}
+          size='thin'
+          width='90%'
+          secondary='$50.00'
+          dual>
+          Video Team
+        </Button>
+        <Button
+          color={props.color}
+          size='thin'
+          width='90%'
+          secondary='$50.00'
+          dual>
+          Cirriculum
+        </Button>
+      </div>
+      {addButton}
+    </>
+  )
+
+  if (showModal) {
+    content = (
+      <Modal color={props.color} clicked={closeModal}></Modal>
+    )
   }
 
   return (
@@ -17,36 +69,7 @@ const Summary = props => {
       height={props.height || '100%'}
       width={props.width ? props.width : '33%'}>
       <div className={classes.Summary}>
-        <h1 className={props.color}>
-          {props.title ? props.title : 'Types'}
-        </h1>
-        <div className={props.canAdd ? classes.Buttons : classes.FullButtons}>
-          <Button
-            color={props.color}
-            size='thin'
-            width='90%'
-            secondary='$89.32'
-            dual>
-            Web Design
-          </Button>
-          <Button
-            color={props.color}
-            size='thin'
-            width='90%'
-            secondary='$50.00'
-            dual>
-            Video Team
-          </Button>
-          <Button
-            color={props.color}
-            size='thin'
-            width='90%'
-            secondary='$50.00'
-            dual>
-            Cirriculum
-          </Button>
-        </div>
-        {addButton}
+        {content}
       </div>
     </Container>
   )
