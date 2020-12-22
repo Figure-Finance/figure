731ddbc66612e27078aec035c74a8294b8e46fb5
Starlight Romero <starlightromero@gmail.com>
2020-12-21 17:42:38 -0800

    [update] color prop and classes

--- frontend/src/components/UI/Input/Input.js
+++ frontend/src/components/UI/Input/Input.js
@@ -4,19 +4,29 @@ import classes from './Input.module.css'
 
 const Input = props => {
   const [value, setValue] = useState('')
+  const classNames = [classes.Input]
+
+  if (props.color === 'primary') {
+    classNames.push(classes.Primary)
+  } else if (props.color === 'neutral') {
+    classNames.push(classes.Neutral)
+  } else if (props.color === 'danger') {
+    classNames.push(classes.Danger)
+  }
 
   return (
     <input
       type='text'
       value={value}
       placeholder={props.placeholder}
-      className={classes.Input}
+      className={classNames.join(' ')}
       style={{ width: props.width || '100%' }}
       onChange={e => setValue(e.target.value)} />
   )
 }
 
 Input.propTypes = {
+  color: PropTypes.string,
   placeholder: PropTypes.string,
   width: PropTypes.string
 }
