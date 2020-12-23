a86d3848d5775412678c38f18ced379eb3bbe4c1
Starlight Romero <starlightromero@gmail.com>
2020-12-22 18:54:44 -0800

    [update] added custom fill

--- frontend/src/components/Icons/RightArrow/RightArrow.js
+++ frontend/src/components/Icons/RightArrow/RightArrow.js
@@ -2,9 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ReactComponent as Right } from '../../../assets/right.svg'
 
-const RightArrow = props => (
-  <Right style={{ width: '30px', fill: props.fill, marginTop: '6px' }} />
-)
+const RightArrow = props => {
+  let fill
+
+  if (props.fill === 'primary') {
+    fill = '#05F29B'
+  } else if (props.fill === 'neutral') {
+    fill = '#65C8FF'
+  } else if (props.fill === 'danger') {
+    fill = '#FF6565'
+  } else {
+    fill = '#CF79F7'
+  }
+
+  return (
+    <Right style={{ width: '30px', fill: fill, marginTop: '6px' }} />
+  )
+}
 
 RightArrow.propTypes = {
   fill: PropTypes.string
