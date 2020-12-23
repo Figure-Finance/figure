8cd907c95798fd51c0b93c1fc08b412dd4bcb77b
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-23 11:03:14 -0800

    Removed testing console.log statements

--- backend/controllers/dashboard.js
+++ backend/controllers/dashboard.js
@@ -2,17 +2,13 @@ const User = require('../models/user')
 const Finance = require('../models/finances')
 
 exports.getUserFinances = (req, res, next) => {
-  console.log('RUNNING ROUTE')
   User.findOne()
     .then(user => {
-      console.log(`User: ${user} User password: ${user.finances}`)
       Finance.find({ userId: user._id })
         .then(finances => {
-          console.log(`Finances from then block: ${finances}`)
           return res.status(200).json({ msg: 'Getting user finances', finances: finances })
         })
         .catch(err => {
-          console.log('IN CATCH BLOCK')
           console.log(err)
         })
     })
@@ -27,7 +23,6 @@ exports.postUserFinances = (req, res, next) => {
   const description = req.body.description
   const isIncome = req.body.isIncome
   const date = req.body.date
-  console.log(`Finance Data from POST user finances: ${category}`)
   const user = User.findOne()
     .then(user => {
       const finances = new Finance({
@@ -39,7 +34,6 @@ exports.postUserFinances = (req, res, next) => {
         date: date,
         userId: user._id
       })
-      console.log('POSTING')
       finances.save()
       // Needs category and amount as response only
       return res.status(201).json({ user: user, finances: finances })
