b22bc38c0321842273455bf5c7aa5ce121166808
Starlight Romero <starlightromero@gmail.com>
2020-12-23 09:53:35 -0800

    [update] added api call

--- frontend/src/containers/Savings/Savings.js
+++ frontend/src/containers/Savings/Savings.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../api'
 import classes from './Savings.module.css'
 import GraphSummary from '../../components/GraphSummary/GraphSummary'
 import ProgressSummary from '../../components/ProgressSummary/ProgressSummary'
@@ -6,29 +7,25 @@ import Summary from '../../components/Summary/Summary'
 import Navbar from '../../components/Navbar/Navbar'
 
 const Savings = props => {
-  const goals = [
-    {
-      type: 'PS5',
-      amount: 500.00
-    },
-    {
-      type: 'M1 Macbook Pro',
-      amount: 3000.00
-    }
-  ]
+  const [goals, setGoals] = useState([])
+  const [totalGoal, setTotalGoal] = useState(0)
 
-  let totalGoals = 0
-
-  for (const entry of goals) {
-    totalGoals += entry.amount
-  }
+  useEffect(() => {
+    api.get('savings').then(res => {
+      console.log(res.data)
+      setGoals(res.data.itemGoals)
+      setTotalGoal(res.data.bankGoal)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className={classes.Savings}>
       <ProgressSummary
         left='neutral'
         leftAmount={1000.00}
-        rightAmount={totalGoals}
+        rightAmount={totalGoal}
         single />
       <div className={classes.Main}>
         <GraphSummary isSavings />
