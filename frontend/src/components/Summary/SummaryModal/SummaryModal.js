9f1e36dd2f9396e58f86f19ddc19e64ace758a07
Starlight Romero <starlightromero@gmail.com>
2020-12-22 19:02:25 -0800

    [update] added props.isIncome

--- frontend/src/components/Summary/SummaryModal/SummaryModal.js
+++ frontend/src/components/Summary/SummaryModal/SummaryModal.js
@@ -16,7 +16,7 @@ const SummaryModal = props => {
   let isIncome
   let heading
 
-  if (props.color === 'primary') {
+  if (props.isIncome) {
     isIncome = true
     heading = 'Add Income'
   } else {
@@ -91,7 +91,8 @@ const SummaryModal = props => {
 
 SummaryModal.propTypes = {
   clicked: PropTypes.func,
-  color: PropTypes.string
+  color: PropTypes.string,
+  isIncome: PropTypes.bool
 }
 
 export default SummaryModal
