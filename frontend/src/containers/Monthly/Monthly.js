40a3e6a040bbcdcc7d419aeb9295e9a641c7ea15
Starlight Romero <starlightromero@gmail.com>
2020-12-24 18:18:52 -0800

    [update] ChartSummary to Chart

--- frontend/src/containers/Monthly/Monthly.js
+++ frontend/src/containers/Monthly/Monthly.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Monthly.module.css'
 import Progress from '../../components/Progress/Progress'
 import Breakdown from '../../components/Breakdown/Breakdown'
-import ChartSummary from '../../components/ChartSummary/ChartSummary'
+import Chart from '../../components/Chart/Chart'
 import Navbar from '../../components/Navbar/Navbar'
 
 const Monthly = props => {
@@ -54,14 +54,20 @@ const Monthly = props => {
   return (
     <div className={classes.Monthly}>
       <Progress
-        left='primary'
+        leftColor='primary'
         leftAmount={totalIncome}
-        right='danger'
+        rightColor='danger'
         rightAmount={totalExpenses} />
       <div className={classes.Main}>
-        <Breakdown content={income} color='primary' />
-        <ChartSummary title='Month' data={expenses} />
-        <Breakdown content={expenses} color='danger' />
+        <Breakdown
+          title='Income'
+          content={income}
+          color='primary' />
+        <Chart title='Month' data={expenses} />
+        <Breakdown
+          title='Expenses'
+          content={expenses}
+          color='danger' />
       </div>
       <Navbar active='m' />
     </div>
