756905181069e33fb9c6a777292effd8201a021b
Starlight Romero <starlightromero@gmail.com>
2020-12-25 19:40:23 -0800

    [update] Navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,39 +4,37 @@ import classes from './Navbar.module.css'
 import Container from '../UI/Container/Container'
 import NavItem from './NavItem/NavItem'
 
-const Navbar = props => {
-  return (
-    <div className={classes.Navbar}>
-      <Container>
-        <NavItem
-          link='/'
-          active={props.active === 'w'}>
-          W
-        </NavItem>
-        <NavItem
-          link='/monthly'
-          active={props.active === 'm'}>
-          M
-        </NavItem>
-        <NavItem
-          link='/yearly'
-          active={props.active === 'y'}>
-          Y
-        </NavItem>
-        <NavItem
-          link='/savings'
-          active={props.active === 's'}>
-          S
-        </NavItem>
-        <NavItem
-          link='/profile'
-          active={props.active === 'p'}>
-          P
-        </NavItem>
-      </Container>
-    </div>
-  )
-}
+const Navbar = props => (
+  <div className={classes.Navbar}>
+    <Container>
+      <NavItem
+        link='/'
+        active={props.active === 'w'}>
+        W
+      </NavItem>
+      <NavItem
+        link='/monthly'
+        active={props.active === 'm'}>
+        M
+      </NavItem>
+      <NavItem
+        link='/yearly'
+        active={props.active === 'y'}>
+        Y
+      </NavItem>
+      <NavItem
+        link='/savings'
+        active={props.active === 's'}>
+        S
+      </NavItem>
+      <NavItem
+        link='/profile'
+        active={props.active === 'p'}>
+        P
+      </NavItem>
+    </Container>
+  </div>
+)
 
 Navbar.propTypes = {
   active: PropTypes.string
