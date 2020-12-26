cc99a1d30dd5e2647389a46bf72956286a3367b9
Starlight Romero <starlightromero@gmail.com>
2020-12-26 08:25:37 -0800

    [update] Navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,52 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './Navbar.module.css'
 import Container from '../UI/Container/Container'
-import NavItem from './NavItem/NavItem'
+import NavSavings from './NavSavings/NavSavings'
+import Nav from './Nav/Nav'
 
-const Navbar = props => (
-  <div className={classes.Navbar}>
+const Navbar = props => {
+  let nav = (
     <Container>
-      <NavItem
-        color='primary'
-        size='square'
-        link='/'
-        active={props.active === 'w'}>
-        W
-      </NavItem>
-      <NavItem
-        color='primary'
-        size='square'
-        link='/monthly'
-        active={props.active === 'm'}>
-        M
-      </NavItem>
-      <NavItem
-        color='primary'
-        size='square'
-        link='/yearly'
-        active={props.active === 'y'}>
-        Y
-      </NavItem>
-      <NavItem
-        color='primary'
-        size='square'
-        link='/savings'
-        active={props.active === 's'}>
-        S
-      </NavItem>
-      <NavItem
-        color='primary'
-        size='square'
-        link='/profile'
-        active={props.active === 'p'}>
-        P
-      </NavItem>
+      <Nav active={props.active} style={{ width: '500px' }} />
     </Container>
-  </div>
-)
+  )
+
+  if (props.isSavings) {
+    nav = (
+      <Container borderRadius='20px'>
+        <NavSavings
+          navSavingsChange={props.onNavSavingsChange}
+          active={props.active}
+          style={{ width: '400px' }} />
+      </Container>
+    )
+  }
+
+  return (
+    <div className={classes.Navbar}>
+      {nav}
+    </div>
+  )
+}
 
 Navbar.propTypes = {
-  active: PropTypes.string
+  active: PropTypes.string,
+  isSavings: PropTypes.bool,
+  onNavSavingsChange: PropTypes.func
 }
 
 export default Navbar
