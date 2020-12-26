6d386738f9a650f52fd0171f820708d240255d68
Starlight Romero <starlightromero@gmail.com>
2020-12-25 20:01:50 -0800

    [update] Graph component

--- frontend/src/components/Graph/Graph.js
+++ frontend/src/components/Graph/Graph.js
@@ -1,53 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Container from '../UI/Container/Container'
-import GraphSummary from './GraphSummary/GraphSummary'
-import GraphModal from './GraphModal/GraphModal'
+import GraphYearly from './GraphYearly/GraphYearly'
+import GraphSavings from './GraphSavings/GraphSavings'
 
 const Graph = props => {
-  const [showModal, setShowModal] = useState(false)
-  const [currentIndex, setCurrentIndex] = useState(props.timePeriods.length - 1)
-
-  const openModalHandler = () => {
-    setShowModal(true)
-  }
-
-  const changeSelection = event => {
-    const index = props.timePeriods.findIndex(el => el === event.target.innerHTML)
-    setCurrentIndex(index)
-    setShowModal(false)
-  }
-
-  const decrementNumber = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1)
-    }
-  }
-
-  const incrementNumber = () => {
-    if (currentIndex < currentIndex.length - 1) {
-      setCurrentIndex(currentIndex + 1)
-    }
-  }
-
-  const currentTimePeriod = props.timePeriods[currentIndex].toString()
-
   let content = (
-    <GraphSummary
-      openModal={openModalHandler}
-      isSavings={props.isSavings}
-      showYearly={props.showYearly}
-      buttonContent={currentTimePeriod}
-      incrementNumber={incrementNumber}
-      decrementNumber={decrementNumber} />
+    <GraphYearly timePeriods={props.timePeriods} />
   )
 
-  if (showModal) {
-    content = (
-      <GraphModal
-        years={props.timePeriods}
-        onClick={changeSelection} />
-    )
+  if (props.isSavings) {
+    content = <GraphSavings />
   }
 
   return (
@@ -58,7 +21,6 @@ const Graph = props => {
 }
 
 Graph.propTypes = {
-  showYearly: PropTypes.bool,
   isSavings: PropTypes.bool,
   timePeriods: PropTypes.arrayOf(PropTypes.string)
 }
