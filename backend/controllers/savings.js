53e3f629a7dec6048b3de33a15fe60e1970726e2
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-26 20:15:29 -0800

    Finished savings/:timeFrame route. Added comments and cleaned up function

--- backend/controllers/savings.js
+++ backend/controllers/savings.js
@@ -6,12 +6,17 @@ const timeFrameUtils = require('../util/savingsByTimeFrame')
 
 const startOfToday = require('date-fns/startOfToday')
 
+const isWithinInterval = require('date-fns/isWithinInterval')
+
+const eachDayOfInterval = require('date-fns/eachDayOfInterval')
 const startOfWeek = require('date-fns/startOfWeek')
 const lastDayOfWeek = require('date-fns/lastDayOfWeek')
 
+const eachMonthOfInterval = require('date-fns/eachMonthOfInterval')
 const startOfYear = require('date-fns/startOfYear')
 const lastDayOfYear = require('date-fns/lastDayOfYear')
 
+const eachQuarterOfInterval = require('date-fns/eachQuarterOfInterval')
 const startOfQuarter = require('date-fns/startOfQuarter')
 const lastDayOfQuarter = require('date-fns/lastDayOfQuarter')
 
@@ -19,6 +24,7 @@ const eachWeekOfInterval = require('date-fns/eachWeekOfInterval')
 const startOfMonth = require('date-fns/startOfMonth')
 const lastDayOfMonth = require('date-fns/lastDayOfMonth')
 
+const { filterByTimeFrame, filterSavingsData } = timeFrameUtils
 // TOTAL SAVINGS ROUTES
 
 exports.getSavings = (req, res, next) => {
@@ -99,46 +105,79 @@ exports.getByTimeFrame = (req, res, next) => {
   const timeFrame = req.params.timeFrame
   let interval
   let savings
-  let savingsToReturn
+  let savingsToReturn = []
   // TODO: find by current user id
   Savings.findOne()
     .then(savingsItem => {
       if (timeFrame === 'month') {
-        let savingsToReturn = []
-        savings = timeFrameUtils.filterByTimeFrame('month', savingsItem.progressUpdates, startOfMonth, lastDayOfMonth)
+        // If user clicks monthly view, filter to show their progress data by week
+        savings = filterByTimeFrame('month', savingsItem.progressUpdates, startOfMonth, lastDayOfMonth)
         if (savings) {
+          // Use each week of interval as a helper
           weeks = eachWeekOfInterval({
             start: savings[0].date,
             end: savings[savings.length - 1].date
           })
         }
-        console.log(timeFrameUtils.filterSavingsData(savings, timeFrame, weeks.length, lastDayOfWeek, savingsToReturn, weeks, 0))
-        // Get weeks in interval, return dates within those weeks?
-        // [{period: week1, amount: cumTotal}, ...]
-        // So we could use the cumulative total from the last day of each week, but how do we figure out
-        // What the last day in the accurate weeks are?
+        // Use filterSavingsData from utils to return data in correct format for FE
+        savingsToReturn = filterSavingsData(savings, 'Week ', weeks.length, lastDayOfWeek, savingsToReturn, weeks, 0)
       } else if (timeFrame === 'year') {
-        savings = timeFrameUtils.filterByTimeFrame('year', savingsItem.progressUpdates, startOfYear, lastDayOfYear)
+        // If user clicks yearly view, filter to show their progress data by month
+        savings = filterByTimeFrame('year', savingsItem.progressUpdates, startOfYear, lastDayOfYear)
+        if (savings) {
+          // Use each month of interval as a helper
+          months = eachMonthOfInterval({
+            start: savings[0].date,
+            end: savings[savings.length - 1].date
+          })
+        }
+        // Use filterSavingsData from utils to return data in correct format for FE
+        savingsToReturn = filterSavingsData(savings, 'Month ', months.length, lastDayOfMonth, savingsToReturn, months, 0)
       } else if (timeFrame === 'week') {
-        savings = timeFrameUtils.filterByTimeFrame('week', savingsItem.progressUpdates, startOfWeek, lastDayOfWeek)
+        // If user clicks weekly view, filter to show their progress data by day
+        console.log(savingsItem)
+        savings = filterByTimeFrame('week', savingsItem.progressUpdates, startOfWeek, lastDayOfWeek)
+        console.log(savings)
+        if (savings) {
+          // Use each week of interval as a helper
+          days = eachDayOfInterval({
+            start: savings[0].date,
+            end: savings[savings.length - 1].date
+          })
+        }
+        // Use filterSavingsData from utils to return data in correct format for FE
+        savingsToReturn = filterSavingsData(savings, 'Day ', days.length, lastDayOfWeek, savingsToReturn, days, 0)
       } else if (timeFrame === 'all') {
-        // TODO: edit this to make sense for 'all's arbitrary start date
-        savings = updatesList.filter(i => {
+        // If user clicks all view, filter to show their progress data by quarter
+        savings = savingsItem.progressUpdates.filter(i => {
           return isWithinInterval(new Date(i.date), {
             // TODO: make start date based on user creation date
             start: new Date(2019, 01, 01),
             end: startOfToday()
           })
         })
+        if (savings) {
+          // Use each week of interval as a helper
+          quarters = eachQuarterOfInterval({
+            start: savings[0].date,
+            end: savings[savings.length - 1].date
+          })
+        }
+        // Use filterSavingsData from utils to return data in correct format for FE
+        savingsToReturn = filterSavingsData(savings, 'Quarter ', quarters.length, lastDayOfQuarter, savingsToReturn, quarters, 0)
       } else if (timeFrame === 'quarter') {
+        // If user clicks quarterly view, filter to show their progress data by week
         savings = timeFrameUtils.filterByTimeFrame('quarter', savingsItem.progressUpdates, startOfQuarter, lastDayOfQuarter)
+        if (savings) {
+          // Use each week of interval as a helper
+          weeks = eachWeekOfInterval({
+            start: savings[0].date,
+            end: savings[savings.length - 1].date
+          })
+        }
+        // Use filterSavingsData from utils to return data in correct format for FE
+        savingsToReturn = filterSavingsData(savings, 'Week ', weeks.length, lastDayOfWeek, savingsToReturn, weeks, 0)
       }
-      return res.status(200).json(savings)
+      return res.status(200).json(savingsToReturn)
     })
-
-// By week: display each day
-// By month: display each week
-// By 3 month: display each week
-// By year: display each month
-// By all time: display quarterly
 }
--- backend/util/savingsByTimeFrame.js
+++ backend/util/savingsByTimeFrame.js
@@ -1,8 +1,13 @@
 const isWithinInterval = require('date-fns/isWithinInterval')
 const startOfToday = require('date-fns/startOfToday')
+const isAfter = require('date-fns/isAfter')
 
-exports.filterByTimeFrame = (timeFrame, updatesList, startOfTimeFrameFunc, endOfTimeFrameFunc) => {
+const filterByTimeFrame = (timeFrame, updatesList, startOfTimeFrameFunc, endOfTimeFrameFunc) => {
+  // Take updates list and return a filtered list
   savings = updatesList.filter(i => {
+    // isWithinInterval returns a boolean
+    // Check if each item is within the interval (between the start and end dates provided)
+    // Return if true
     return isWithinInterval(new Date(i.date), {
       start: startOfTimeFrameFunc(startOfToday()),
       end: endOfTimeFrameFunc(startOfToday())
@@ -11,40 +16,41 @@ exports.filterByTimeFrame = (timeFrame, updatesList, startOfTimeFrameFunc, endOf
   return savings
 }
 
-exports.filterSavingsData = (savings, timeFrame, indeces, lastDayFunc, returnList, periodList, iterator) => {
+const filterSavingsData = (savings, period, indeces, lastDayFunc, returnList, periodList, iterator) => {
+  // Create a helper cursor variable
   let cursor
+  // Create helper i variable that begins at the index of iterator (parameter passed in initially as zero)
   const i = [...savings][iterator]
-  // console.log(`i from before base if: ${i}`)
-  // console.log(`period list: ${periodList}`)
-  // console.log(`Indeces before base case if statement: ${indeces}`)
-  if (iterator === indeces) {
-    console.log(`Base case iterator: ${iterator}`)
-    console.log(`Return list: ${returnList}`)
-    return returnList
-  }
-  if (periodList.length > 1 && periodList[iterator].includes(i.date.toString()) ||
-      periodList.length === 1 && periodList[0] === i.date.toString()) {
-    if (i.date.toString() === periodList[iterator].lastDayFunc().toString()) {
-      returnList.push({ period: 'timeFrame' + (iterator + 1), amount: i.curTotal })
-      iterator++
-      return filterSavingsData(savings, timeFrame, indeces, lastDayFunc, returnList, periodList, iterator)
-    } else {
-      // TODO: These are making the recursion exit too early - figure it out
-      cursor = i.date
-      iterator++
-      console.log('iterator from first else')
-      console.log(`Cursor from first else ${cursor}`)
-      if (i.date > cursor) {
+  // Use our indeces (number of periods) as a base case to end the loop
+  while (iterator <= indeces) {
+    if (periodList[iterator]) {
+      // If item.date.toString (to be able to truly check equality) = the last day in the given period, we want that date's total
+      // So we append it to our returnList
+      if (i.date.toString() === lastDayFunc(periodList[iterator]).toString()) {
+        returnList.push({ period: period + (iterator + 1), amount: i.curTotal })
+        return filterSavingsData(savings, period, indeces, lastDayFunc, returnList, periodList, iterator + 1)
+      // If it isn't exactly the last day of the week, month, etc. we need to find out what the latest date in that period is
+      } else if (i.date.toString() !== lastDayFunc(periodList[iterator]).toString()) {
+        // Set the cursor variable to the item's date
         cursor = i.date
-        iterator++
-        console.log(`Cursor from if (i.date is greater than): ${cursor}`)
+        if (isAfter(new Date(i.date), new Date(cursor))) {
+          // If this re-executes and the new item's date is after the current cursor, we make that new date the cursor
+          cursor = i.date
+        } else {
+          // Eventually, when we have the latest date we can, we append this date and total to the returnList
+          returnList.push({ period: period + (iterator + 1), amount: cursor.curTotal })
+          return filterSavingsData(savings, period, indeces, lastDayFunc, returnList, periodList, iterator + 1)
+        }
       } else {
-        returnList.push({ period: 'timeFrame' + (iterator + 1), amount: cursor.curTotal })
-        iterator++
-        console.log(`Return list from last else: ${returnList}`)
-        console.log(`Iterator from last else: ${iterator}`)
-        return filterSavingsData(savings, timeFrame, indeces, lastDayFunc, returnList, periodList, iterator)
+        // TODO: probably remove this statement? I don't think it's necessary and I think it's fucking things up
+        break
       }
     }
+    // Increment the iterator so we don't end up in an infinite loop
+    iterator++
   }
+  return returnList
 }
+
+module.exports.filterSavingsData = filterSavingsData
+module.exports.filterByTimeFrame = filterByTimeFrame
