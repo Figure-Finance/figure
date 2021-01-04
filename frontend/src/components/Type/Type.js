b4aa0d5088eb340976ab3e84f58d55e5fe121748
Starlight Romero <starlightromero@gmail.com>
2021-01-03 22:57:37 -0800

    [create] Type component

---
+++ frontend/src/components/Type/Type.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './Type.module.css'
+import Container from '../UI/Container/Container'
+import Button from '../UI/Button/Button'
+
+const Type = props => {
+  const buttons = props.content.map(item => {
+    return (
+      <Button
+        key={item}
+        onClick={() => props.openDetailModal(item)}
+        color={props.color}
+        size='thin'
+        width='90%'>
+        {item}
+      </Button>
+    )
+  })
+
+  return (
+    <Container
+      height='100%'
+      width='33%'>
+      <div className={classes.Type}>
+        {buttons}
+      </div>
+    </Container>
+  )
+}
+
+Type.propTypes = {
+  content: PropTypes.arrayOf(PropTypes.string),
+  color: PropTypes.string,
+  openDetailModal: PropTypes.func
+}
+
+export default Type
