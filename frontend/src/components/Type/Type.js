9095139cfbbdbd72e4436d60fda83acf2ef6076b
Starlight Romero <starlightromero@gmail.com>
2021-01-05 09:28:54 -0800

    [update] Type component

--- frontend/src/components/Type/Type.js
+++ frontend/src/components/Type/Type.js
@@ -1,36 +1,76 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import classes from './Type.module.css'
 import Container from '../UI/Container/Container'
-import Button from '../UI/Button/Button'
-import AddButton from '../UI/AddButton/AddButton'
+import TypeSummary from './TypeSummary/TypeSummary'
+import TypeAddModal from './TypeAddModal/TypeAddModal'
+import TypeDetailModal from './TypeDetailModal/TypeDetailModal'
 
 const Type = props => {
-  const buttons = props.content.map(item => {
-    return (
-      <Button
-        key={item}
-        onClick={() => props.openDetailModal(item)}
+  const [showAddModal, setShowAddModal] = useState(false)
+  const [showDetailModal, setShowDetailModal] = useState(false)
+  const [currentItem, setCurrentItem] = useState(null)
+
+  const openAddModalHandler = useCallback(() => {
+    setShowAddModal(true)
+  }, [])
+
+  const closeAddModalHandler = useCallback(() => {
+    setShowAddModal(false)
+  }, [])
+
+  const openDetailModalHandler = useCallback(id => {
+    const selectedItem = props.content.filter(item => item.id === id)
+    setCurrentItem(selectedItem[0])
+    setShowDetailModal(true)
+  }, [props.content])
+
+  const closeDetailModalHandler = useCallback(() => {
+    setShowDetailModal(false)
+  }, [])
+
+  const { addGoal, deleteGoal } = props
+
+  const addGoalHandler = useCallback(goal => {
+    addGoal(goal, res => {
+      setShowAddModal(false)
+    })
+  }, [addGoal])
+
+  const deleteGoalHandler = useCallback(id => {
+    deleteGoal(id, res => {
+      setShowDetailModal(false)
+    })
+  }, [deleteGoal])
+
+  let content = (
+    <TypeSummary
+      color={props.color}
+      content={props.content}
+      openDetailModal={() => openDetailModalHandler(currentItem.id)}
+      openAddModal={openAddModalHandler} />
+  )
+
+  if (showAddModal) {
+    content = (
+      <TypeAddModal
         color={props.color}
-        size='thin'
-        width='90%'>
-        {item}
-      </Button>
+        closeModal={closeAddModalHandler}
+        onSubmit={addGoalHandler} />
     )
-  })
+  } else if (showDetailModal) {
+    content = (
+      <TypeDetailModal
+        color={props.color}
+        onDelete={() => deleteGoalHandler(currentItem.id)}
+        closeModal={closeDetailModalHandler} />
+    )
+  }
 
   return (
     <Container
       height='100%'
       width='33%'>
-      <div className={classes.Type}>
-        <div className={classes.Buttons}>
-          {buttons}
-        </div>
-        <div className={classes.Add}>
-          <AddButton color={props.color} />
-        </div>
-      </div>
+      {content}
     </Container>
   )
 }
@@ -38,7 +78,8 @@ const Type = props => {
 Type.propTypes = {
   content: PropTypes.arrayOf(PropTypes.string),
   color: PropTypes.string,
-  openDetailModal: PropTypes.func
+  addGoal: PropTypes.func,
+  deleteGoal: PropTypes.func
 }
 
 export default Type
