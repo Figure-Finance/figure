71bc497b8b8ed3bd2dcca101acba9ad760cf3a7f
Starlight Romero <starlightromero@gmail.com>
2021-01-05 09:30:00 -0800

    [create] TypeSummary component

---
+++ frontend/src/components/Type/TypeSummary/TypeSummary.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './TypeSummary.module.css'
+import Button from '../../UI/Button/Button'
+import AddButton from '../../UI/AddButton/AddButton'
+
+const TypeSummary = props => {
+  const buttons = props.content.map(item => {
+    return (
+      <Button
+        key={item}
+        onClick={() => props.openDetailModal(item)}
+        color={props.color}
+        size='thin'
+        width='90%'>
+        {item}
+      </Button>
+    )
+  })
+
+  return (
+    <div className={classes.Type}>
+      <div className={classes.Buttons}>
+        {buttons}
+      </div>
+      <div className={classes.Add}>
+        <AddButton
+          color={props.color}
+          onClick={props.openAddModal} />
+      </div>
+    </div>
+  )
+}
+
+TypeSummary.propTypes = {
+  content: PropTypes.arrayOf(PropTypes.string),
+  color: PropTypes.string,
+  openDetailModal: PropTypes.func,
+  openAddModal: PropTypes.func
+}
+
+export default TypeSummary
