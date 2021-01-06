c08533a3609ba01b271c0b80f47e9e4c6ecf1d05
Starlight Romero <starlightromero@gmail.com>
2021-01-06 15:18:23 -0800

    [update] Graph if else statement

--- frontend/src/components/Graph/GraphSummary/Graph/Graph.js
+++ frontend/src/components/Graph/GraphSummary/Graph/Graph.js
@@ -3,49 +3,7 @@ import { Line } from 'react-chartjs-2'
 import PropTypes from 'prop-types'
 
 const Graph = props => {
-  let graph = (
-    <Line
-      data={{
-        labels: props.labels,
-        datasets: [
-          {
-            label: 'Income',
-            data: props.data.income,
-            backgroundColor: [
-              'rgba(5, 242, 155, 0.2)'
-            ],
-            borderColor: [
-              '#05F29B'
-            ],
-            borderWidth: 2,
-            pointStyle: 'rectRounded',
-            pointBackgroundColor: '#05F29B',
-            pointBorderColor: '#05F29B',
-            pointRadius: '4',
-            pointHoverRadius: '10'
-          },
-          {
-            label: 'Expenses',
-            data: props.data.expenses,
-            backgroundColor: [
-              'rgba(255, 101, 101, 0.2)'
-            ],
-            borderColor: [
-              '#FF6565'
-            ],
-            borderWidth: 2,
-            pointStyle: 'rectRounded',
-            pointBackgroundColor: '#FF6565',
-            pointBorderColor: '#FF6565',
-            pointRadius: '4',
-            pointHoverRadius: '10'
-          }
-        ]
-      }}
-      height={370}
-      width={600}
-      options={{ maintainAspctRatio: false }} />
-  )
+  let graph
 
   if (props.isSavings) {
     graph = (
@@ -66,6 +24,50 @@ const Graph = props => {
               pointStyle: 'rectRounded',
               pointBackgroundColor: '#65C8FF',
               pointBorderColor: '#65C8FF',
+              pointRadius: '4',
+              pointHoverRadius: '10'
+            }
+          ]
+        }}
+        height={370}
+        width={600}
+        options={{ maintainAspctRatio: false }} />
+    )
+  } else {
+    graph = (
+      <Line
+        data={{
+          labels: props.labels,
+          datasets: [
+            {
+              label: 'Income',
+              data: props.data.income,
+              backgroundColor: [
+                'rgba(5, 242, 155, 0.2)'
+              ],
+              borderColor: [
+                '#05F29B'
+              ],
+              borderWidth: 2,
+              pointStyle: 'rectRounded',
+              pointBackgroundColor: '#05F29B',
+              pointBorderColor: '#05F29B',
+              pointRadius: '4',
+              pointHoverRadius: '10'
+            },
+            {
+              label: 'Expenses',
+              data: props.data.expenses,
+              backgroundColor: [
+                'rgba(255, 101, 101, 0.2)'
+              ],
+              borderColor: [
+                '#FF6565'
+              ],
+              borderWidth: 2,
+              pointStyle: 'rectRounded',
+              pointBackgroundColor: '#FF6565',
+              pointBorderColor: '#FF6565',
               pointRadius: '4',
               pointHoverRadius: '10'
             }
