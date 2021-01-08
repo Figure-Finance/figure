b3b7167761fd5da31af1cadb46e37de7919018a4
Starlight Romero <starlightromero@gmail.com>
2021-01-08 07:36:10 -0800

    [update] NavSavings destructure props

--- frontend/src/components/Navbar/NavSavings/NavSavings.js
+++ frontend/src/components/Navbar/NavSavings/NavSavings.js
@@ -3,41 +3,41 @@ import PropTypes from 'prop-types'
 import classes from './NavSavings.module.css'
 import Button from '../../UI/Button/Button'
 
-const NavSavings = props => (
+const NavSavings = ({ active, navSavingsChange }) => (
   <div className={classes.NavSavings}>
     <Button
       color='neutral'
       size='smallSquare'
-      onClick={props.navSavingsChange}
-      active={props.active === '1W'}>
+      onClick={navSavingsChange}
+      active={active === '1W'}>
       1W
     </Button>
     <Button
       color='neutral'
       size='smallSquare'
-      onClick={props.navSavingsChange}
-      active={props.active === '1M'}>
+      onClick={navSavingsChange}
+      active={active === '1M'}>
       1M
     </Button>
     <Button
       color='neutral'
       size='smallSquare'
-      onClick={props.navSavingsChange}
-      active={props.active === '3M'}>
+      onClick={navSavingsChange}
+      active={active === '3M'}>
       3M
     </Button>
     <Button
       color='neutral'
       size='smallSquare'
-      onClick={props.navSavingsChange}
-      active={props.active === '1Y'}>
+      onClick={navSavingsChange}
+      active={active === '1Y'}>
       1Y
     </Button>
     <Button
       color='neutral'
       size='smallSquare'
-      onClick={props.navSavingsChange}
-      active={props.active === 'A'}>
+      onClick={navSavingsChange}
+      active={active === 'A'}>
       A
     </Button>
   </div>
