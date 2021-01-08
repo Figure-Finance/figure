d7e14ca1569cb32f7808de0f10a46e69d6490c7a
Starlight Romero <starlightromero@gmail.com>
2021-01-08 07:36:46 -0800

    [update] Logo remove props

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -2,10 +2,8 @@ import React from 'react'
 import classes from './Logo.module.css'
 import { ReactComponent as LogoIcon } from '../../assets/logo.svg'
 
-const Logo = props => {
-  return (
-    <LogoIcon className={classes.Logo} />
-  )
-}
+const Logo = () => (
+  <LogoIcon className={classes.Logo} />
+)
 
 export default Logo
