01f15f8bd742cb4389d915bc76efaa8b8a69a53e
Starlight Romero <starlightromero@gmail.com>
2021-01-09 07:53:32 -0800

    [update] Savings react-query

--- frontend/src/containers/Savings/Savings.js
+++ frontend/src/containers/Savings/Savings.js
@@ -1,132 +1,78 @@
 import React, { useState, useEffect, useCallback } from 'react'
+import { useQuery } from 'react-query'
 import api from '../../api'
 import classes from './Savings.module.css'
 import Graph from '../../components/Graph/Graph'
 import Progress from '../../components/Progress/Progress'
 import Breakdown from '../../components/Breakdown/Breakdown'
 import Navbar from '../../components/Navbar/Navbar'
+import Loader from '../../components/Loader/Loader'
+import Error from '../../components/Error/Error'
 
 const Savings = () => {
-  const [loading, setLoading] = useState(false)
-  const [goals, setGoals] = useState([])
-  const [goalProgress, setGoalProgress] = useState(0)
-  const [totalGoal, setTotalGoal] = useState(0)
   const [graphTimePeriod, setGraphTimePeriod] = useState('1W')
 
   const onFetchSavings = useCallback(async () => {
-    setLoading(true)
-    try {
-      const res = await api.get('savings')
-      setLoading(false)
-      setGoals(res.data.itemGoals)
-      setGoalProgress(res.data.totalSavingsProgress)
-      setTotalGoal(res.data.totalSavingsGoal)
-    } catch (err) {
-      console.log(err)
-    }
-    setLoading(false)
+    const res = await api.get('savings')
+    return res.data
   }, [])
 
-  const onDeposit = useCallback(async (value, cb) => {
-    try {
-      const res = await api.patch('savings/progress', {
-        progressAmount: value
-      })
-      cb(res.data)
-      setGoalProgress(goalProgress + value)
-    } catch (err) {
-      console.log(err)
-    }
-  }, [goalProgress])
-
-  const onUpdateTotalGoal = useCallback(async (value, cb) => {
-    try {
-      const res = await api.patch('savings/total', {
-        totalSavingsGoal: value
-      })
-      cb(res.data)
-      setTotalGoal(value)
-    } catch (err) {
-      console.log(err)
-    }
+  const onDeposit = useCallback(async progressAmount => {
+    const res = await api.patch('savings/progress', {
+      progressAmount
+    })
+    return res.data
   }, [])
 
-  const onGetGoal = useCallback(async (id, cb) => {
-    try {
-      const res = await api.get(`savings/goal/${id}`)
-      const data = { id, ...res.data }
-      data.amount = +data.amount
-      cb(data)
-    } catch (err) {
-      console.log(err)
-    }
+  const onUpdateTotalGoal = useCallback(async totalSavingsGoal => {
+    const res = await api.patch('savings/total', {
+      totalSavingsGoal
+    })
+    return res.data
   }, [])
 
-  const onAddGoal = useCallback(async (goal, cb) => {
-    try {
-      const res = await api.post('savings/goal', goal)
-      const id = res.data.id
-      const data = { id, progress: 0, ...goal }
-      data.amount = +data.amount
-      setGoals([...goals, data])
-      cb(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }, [goals])
-
-  const onAllocateSavings = useCallback(async (id, allocateAmount, cb) => {
-    try {
-      allocateAmount = +allocateAmount
-      await api.patch('savings/goal/allocate', {
-        id,
-        allocateAmount: allocateAmount
-      })
-      const data = { id, allocateAmount }
-      const updatedGoalIndex = goals.findIndex(goal => goal.id === id)
-      const updatedGoals = [...goals]
-      updatedGoals[updatedGoalIndex].progress += allocateAmount
-      setGoals(updatedGoals)
-      cb(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }, [goals])
-
-  const onUpdateGoal = useCallback(async (id, body, cb) => {
-    try {
-      const res = await api.patch(`savings/goal/${id}`, body)
-      cb(res.data)
-    } catch (err) {
-      console.log(err)
-    }
+  const onGetGoal = useCallback(async id => {
+    const res = await api.get(`savings/goal/${id}`)
+    return res.data
   }, [])
 
-  const onDeleteGoal = useCallback(async (id, cb) => {
-    try {
-      const res = await api.delete(`savings/goal/${id}`)
-      const updatedGoals = goals.filter(goal => goal.id !== id)
-      console.log(res.data)
-      setGoals(updatedGoals)
-      cb(res.data)
-    } catch (err) {
-      console.log(err)
-    }
-  }, [goals])
+  const onAddGoal = useCallback(async newGoal => {
+    newGoal.amount = +newGoal.amount
+    console.log(newGoal)
+    const res = await api.post('savings/goal', newGoal)
+    return res.data
+  }, [])
+
+  const onAllocateSavings = useCallback(async (id, allocateAmount) => {
+    allocateAmount = +allocateAmount
+    const res = await api.patch('savings/goal/allocate', {
+      id,
+      allocateAmount
+    })
+    return res.data
+  }, [])
+
+  const onUpdateGoal = useCallback(async (id, updateGoal) => {
+    const res = await api.patch(`savings/goal/${id}`, updateGoal)
+    return res.data
+  }, [])
+
+  const onDeleteGoal = useCallback(async id => {
+    const res = await api.delete(`savings/goal/${id}`)
+    return res.data
+  }, [])
 
   const onFetchGraphData = useCallback(async timeFrame => {
-    try {
-      const res = await api.get(`savings/progress/${timeFrame}`)
-      console.log(res.data)
-    } catch (err) {
-      console.log(err)
-    }
+    const res = await api.get(`savings/progress/${timeFrame}`)
+    return res.data
   }, [])
 
   useEffect(() => {
     onFetchSavings()
     onFetchGraphData('year')
   }, [onFetchSavings, onFetchGraphData])
+
+  const { data, isLoading, isError } = useQuery('savings', onFetchSavings)
 
   const graphTimePeriodChangeHandler = event => {
     setGraphTimePeriod(event.target.innerHTML)
@@ -142,8 +88,24 @@ const Savings = () => {
     'Sunday'
   ]
 
-  return (
-    <div className={classes.Savings}>
+  let progress
+  let graph
+  let breakdown
+
+  if (isLoading) {
+    progress = <Loader />
+    graph = <Loader />
+    breakdown = <Loader />
+  } else if (isError) {
+    progress = <Error />
+    graph = <Error />
+    breakdown = <Error />
+  } else {
+    const goals = data.itemGoals
+    const goalProgress = data.totalSavingsProgress
+    const totalGoal = data.totalSavingsGoal
+    console.log('goalsProgress', goalProgress)
+    progress = (
       <Progress
         updateGoal={onUpdateTotalGoal}
         updateProgress={onDeposit}
@@ -151,27 +113,37 @@ const Savings = () => {
         leftAmount={goalProgress}
         rightAmount={totalGoal}
         single
-        showButtons
-        loading={loading} />
+        showButtons />
+    )
+    graph = (
+      <Graph
+        onNavSavingsChange={graphTimePeriodChangeHandler}
+        active={graphTimePeriod}
+        labels={labels}
+        isSavings />
+    )
+    breakdown = (
+      <Breakdown
+        allocateSavings={onAllocateSavings}
+        getItem={onGetGoal}
+        addItem={onAddGoal}
+        updateItem={onUpdateGoal}
+        deleteItem={onDeleteGoal}
+        color='neutral'
+        title='Goals'
+        content={goals}
+        canAdd
+        isSavings
+        showButtons />
+    )
+  }
+
+  return (
+    <div className={classes.Savings}>
+      {progress}
       <div className={classes.Main}>
-        <Graph
-          onNavSavingsChange={graphTimePeriodChangeHandler}
-          active={graphTimePeriod}
-          labels={labels}
-          isSavings />
-        <Breakdown
-          allocateSavings={onAllocateSavings}
-          getItem={onGetGoal}
-          addItem={onAddGoal}
-          updateItem={onUpdateGoal}
-          deleteItem={onDeleteGoal}
-          color='neutral'
-          title='Goals'
-          content={goals}
-          loading={loading}
-          canAdd
-          isSavings
-          showButtons />
+        {graph}
+        {breakdown}
       </div>
       <Navbar active='s' />
     </div>
