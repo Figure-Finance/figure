9fd41dd0d4830138ddd18a9138f1064dc0660a0c
Starlight Romero <starlightromero@gmail.com>
2021-01-09 13:19:16 -0800

    [update] api added header

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
-  // baseURL: 'http://localhost:80/api'
 })
+
+// api.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`
 
 export default api
