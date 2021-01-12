e076639da6649d9c765ec071a4aae79895486745
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-12 15:07:24 -0800

    Refactored get yearly graph route to work better for FE

--- backend/controllers/dashboard-yearly.js
+++ backend/controllers/dashboard-yearly.js
@@ -5,43 +5,51 @@ const { filterByTimeFrame, filterSavingsData } = require('../util/savingsByTimeF
 const getMonth = require('date-fns/getMonth')
 const isWithinInterval = require('date-fns/isWithinInterval')
 
-exports.getUserYearlyGraph = (req, res, next) => {
+exports.getUserYearlyGraph = async (req, res, next) => {
   // Return finances for YTD by month for FE graph
   const startDate = new Date(req.params.startDate)
   const endDate = new Date(req.params.endDate)
-  let dates
-  User.findById(req.userId)
-    .then(user => {
-      Finance.aggregate([
-        {
-          $match: {
-            userId: user._id,
-            date: {
-              $gte: startDate,
-              $lte: endDate
-            }
-          }
+  let loadedUser
+  let income
+  let expenses
+  const user = await User.findById(req.userId)
+  const incomeResult = await Finance.aggregate([
+    {
+      $match: {
+        userId: user._id,
+        date: {
+          $gte: startDate,
+          $lte: endDate
         },
-        { $group: { _id: { $month: '$date' }, amount: { $sum: '$amount' }, isIncome: { $first: '$isIncome' } } }
-      ])
-        .exec((err, result) => {
-          if (err) {
-            console.log(err)
-          }
-          if (result) {
-            dates = result.map(r => {
-              return { month: r._id, amount: r.amount, isIncome: r.isIncome }
-            })
-            return res.status(200).json(dates)
-          }
-        })
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500
+        isIncome: true
       }
-      next(err)
-    })
+    },
+    { $group: { _id: { $month: '$date' }, amount: { $sum: '$amount' }, isIncome: { $first: '$isIncome' } } }
+  ])
+    .exec()
+
+  const expensesResult = await Finance.aggregate([
+    {
+      $match: {
+        userId: user._id,
+        date: {
+          $gte: startDate,
+          $lte: endDate
+        },
+        isIncome: false
+      }
+    },
+    { $group: { _id: { $month: '$date' }, amount: { $sum: '$amount' }, isIncome: { $first: '$isIncome' } } }
+  ])
+    .exec()
+
+  income = incomeResult.map(r => {
+    return { month: r._id, amount: r.amount, isIncome: r.isIncome }
+  })
+  expenses = expensesResult.map(r => {
+    return { month: r._id, amount: r.amount, isIncome: r.isIncome }
+  })
+  return res.status(200).json({ income, expenses })
 }
 
 exports.getUserYearlyFinances = (req, res, next) => {
