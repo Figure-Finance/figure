2a2f5a03849417d69b154526077c66c9db0abbe8
Starlight Romero <starlightromero@gmail.com>
2021-01-12 11:55:36 -0800

    [update] Logo with png

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import classes from './Logo.module.css'
-import { ReactComponent as LogoIcon } from '../../assets/logo.svg'
+// import { ReactComponent as LogoIcon } from '../../assets/logo.svg'
+import LogoIcon from '../../assets/logo.png'
 
 const Logo = () => (
-  <LogoIcon className={classes.Logo} />
+  // <LogoIcon className={classes.Logo} />
+  <div classes={classes.Logo}>
+    <img
+      height='400'
+      src={LogoIcon}
+      alt='Figure logo'
+      style={{
+        display: 'block',
+        margin: 'auto'
+      }} />
+  </div>
 )
 
 export default Logo
