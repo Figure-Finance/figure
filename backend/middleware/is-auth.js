0142720f1bdded66686aa28603c43ee8b87635d0
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-27 16:26:06 -0800

    Removed phantom console.log() statement.

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization')
   if (!authHeader) {
-    console.log()
     const error = new Error('Not authenticated.')
     error.statusCode = 401
     throw error
