1fbc9fba25ac5f25879dd81789e499ca0b778cd9
Starlight Romero <starlightromero@protonmail.com>
2021-03-27 00:46:00 -0700

    [update] App add Home container routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Auth from './containers/Auth/Auth'
 import Questions from './containers/Questions/Questions'
+import Home from './containers/Home/Home'
 import Weekly from './containers/Weekly/Weekly'
 import Monthly from './containers/Monthly/Monthly'
 import Yearly from './containers/Yearly/Yearly'
@@ -11,13 +12,14 @@ import Profile from './containers/Profile/Profile'
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/auth" component={Auth} />
       <Route exact path="/auth/questions" component={Questions} />
+      <Route exact path="/auth" component={Auth} />
       <Route exact path="/profile" component={Profile} />
       <Route exact path="/savings" component={Savings} />
       <Route exact path="/yearly" component={Yearly} />
       <Route exact path="/monthly" component={Monthly} />
-      <Route exact path="/" component={Weekly} />
+      <Route exact path="/weekly" component={Weekly} />
+      <Route exact path="/" component={Home} />
       <Redirect to="/" />
     </Switch>
   )
