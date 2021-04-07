6bd835d65f6a30eec39c310342458666ac939b4a
Starlight Romero <starlightromero@protonmail.com>
2021-04-07 11:57:56 -0700

    [update] Home container

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -4,37 +4,50 @@ import { NavLink } from 'react-router-dom'
 import classes from './Home.module.css'
 import Logo from '../../components/Logo/Logo'
 import Button from '../../components/UI/Button/Button'
+import Container from '../../components/UI/Container/Container'
 
 const Home = ({ history }) => {
   return (
     <div className={classes.Home}>
-      <div className={classes.HeadBar}>
-        <div className={classes.Brand}>
-          <Logo height={64} />
-          <div className={classes.BrandName}>
-            <h2 className="primary">Figure</h2>
-            <h2 className="primary">Finance</h2>
+      <Container width="98%" height="80px">
+        <div className={classes.HeadBar}>
+          <div className={classes.Brand}>
+            <Logo height={64} />
+            <div className={classes.BrandName}>
+              <h2 className="primary">Figure</h2>
+              <h2 className="primary">Finance</h2>
+            </div>
+          </div>
+          <div className={classes.Auth}>
+            <NavLink
+              to={{
+                pathname: '/auth',
+                state: { isSignUp: false },
+              }}>
+              <Button color="primary" size="small" type="sumbit">
+                Sign In
+              </Button>
+            </NavLink>
+            <NavLink
+              to={{
+                pathname: '/auth',
+                state: { isSignUp: true },
+              }}>
+              <Button color="primary" size="small" type="sumbit">
+                Sign Up
+              </Button>
+            </NavLink>
           </div>
         </div>
-        <div className={classes.Auth}>
-          <NavLink to={'/auth'}>
-            <Button color="primary" size="small" type="sumbit">
-              Sign In
-            </Button>
-          </NavLink>
-          <NavLink to={'/auth'}>
-            <Button color="primary" size="small" type="sumbit">
-              Sign Up
-            </Button>
-          </NavLink>
-        </div>
-      </div>
+      </Container>
       <div className={classes.Main}>
-        <div className={`${classes.Title} primary`}>Figure</div>
-        <h1 className="primary">
-          The new way to <em>refigure</em> your life
-        </h1>
-        <h2 className="primary">Take your finances to a new level</h2>
+        <Container width="650px" height="430px">
+          <div className={`${classes.Title} primary`}>Figure</div>
+          <h1 className="primary">
+            The new way to <em>refigure</em> your life
+          </h1>
+          <h2 className="primary">Take your finances to a new level</h2>
+        </Container>
       </div>
     </div>
   )
