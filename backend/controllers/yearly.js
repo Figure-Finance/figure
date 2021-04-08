c6fe0ceb1a89777b8a0a326a2a6e60ff65bf51d9
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-08 14:40:11 -0700

    [update] Yearly and yearly-graph routes updated to remove pre-auth flow code.

--- backend/controllers/yearly.js
+++ backend/controllers/yearly.js
@@ -5,11 +5,10 @@ exports.getUserYearlyGraph = async (req, res, next) => {
   // Return finances for YTD by month for FE graph
   const startDate = new Date(req.params.startDate)
   const endDate = new Date(req.params.endDate)
-  const user = await User.findById(req.userId)
   const incomeResult = await Finance.aggregate([
     {
       $match: {
-        userId: user._id,
+        userId: req.userId,
         date: {
           $gte: startDate,
           $lte: endDate
@@ -29,7 +28,7 @@ exports.getUserYearlyGraph = async (req, res, next) => {
   const expensesResult = await Finance.aggregate([
     {
       $match: {
-        userId: user._id,
+        userId: req.userId,
         date: {
           $gte: startDate,
           $lte: endDate
@@ -59,41 +58,32 @@ exports.getUserYearlyFinances = (req, res, next) => {
   const startDate = new Date(req.params.startDate)
   const endDate = new Date(req.params.endDate)
   let finances
-  User.findById(req.userId)
-    .then((user) => {
-      Finance.aggregate([
-        {
-          $match: {
-            userId: user._id,
-            date: {
-              $gte: startDate,
-              $lte: endDate
-            }
-          }
-        },
-        {
-          $group: {
-            _id: '$category',
-            amount: { $sum: '$amount' },
-            isIncome: { $first: '$isIncome' }
-          }
+  Finance.aggregate([
+    {
+      $match: {
+        userId: req.userId,
+        date: {
+          $gte: startDate,
+          $lte: endDate
         }
-      ]).exec((err, result) => {
-        if (err) {
-          console.log(err)
-        }
-        if (result) {
-          finances = result.map((r) => {
-            return { category: r._id, amount: r.amount, isIncome: r.isIncome }
-          })
-          return res.status(200).json(finances)
-        }
-      })
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500
       }
-      next(err)
-    })
+    },
+    {
+      $group: {
+        _id: '$category',
+        amount: { $sum: '$amount' },
+        isIncome: { $first: '$isIncome' }
+      }
+    }
+  ]).exec((err, result) => {
+    if (err) {
+      console.log(err)
+    }
+    if (result) {
+      finances = result.map((r) => {
+        return { category: r._id, amount: r.amount, isIncome: r.isIncome }
+      })
+      return res.status(200).json(finances)
+    }
+  })
 }
